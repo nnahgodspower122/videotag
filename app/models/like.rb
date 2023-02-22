9a131cf5e1eb300e70ae1d82a3bf61276d20985c
gru360vin <vin_kim@cw.groups360.com>
2023-02-22 14:09:53 -0500

    revert like migration

--- app/models/like.rb
+++
@@ -1,5 +0,0 @@
-class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
-  validates :user_id, uniqueness: { scope: :post_id }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_214801) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_185539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -73,7 +63,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_214801) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
