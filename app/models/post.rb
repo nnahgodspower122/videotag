6bf8745c2ef1210f9759e033426c7d86075204b3
gru360vin <vin_kim@cw.groups360.com>
2023-02-24 17:50:31 -0500

    added friendly_id finders and history

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,11 @@ class Post < ApplicationRecord
   has_many :likes
 
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :title, use: [:slugged, :finders, :history]
 
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
 
   scope :free, -> {where(premium: false)}
   scope :premium, -> {where(premium: true)}
